dd0baac25c0c4bd5e93bb5280d4fdb3ea10f644f
sumit7200 <rajsumit7200@gmail.com>
2024-11-25 18:51:31 +0530

    first commit

---
+++ src/main/java/com/example/school/model/Studentt.java
@@ -0,0 +1,57 @@
+package com.example.school.model;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Studentt {
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private long id;
+    private String name;
+    private String section;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
+
+    public Studentt(long id, String name, String section) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.section = section;
+    }
+
+    public Studentt() {
+        super();
+    }
+
+    @Override
+    public String toString() {
+        return "Studentt [id=" + id + ", name=" + name + ", section=" + section + "]";
+    }
+
+}
